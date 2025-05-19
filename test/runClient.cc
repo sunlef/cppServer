bb3cf9590adae09b2a8213e755253da6420b0840
sunlef <cleanblue@DESKTOP-HPPTJIG.>
2025-05-20 05:45:45 +0800

    从配置文件读取服务器ip

--- test/runClient.cc
+++ test/runClient.cc
@@ -1,16 +1,37 @@
 
 #include "client.h"
+#include <cstring>
 #include <iostream>
+#include <fstream>
 
 int main() {
   using namespace cppServer;
 
+  std::ifstream ipFile("config/server_ip.txt");
+
+  if (!ipFile.is_open()) {
+    std::cerr << "Failed to open server_ip.txt" << std::endl;
+    return 1;
+  }
+
+  std::string ip;
+  std::getline(ipFile, ip);
+  if (ip.empty()) {
+    std::cerr << "server_ip.txt is empty" << std::endl;
+    return 1;
+  }
+  ipFile.close();
+
+  char *server_ip = new char[ip.size() + 1];
+  std::strcpy(server_ip, ip.c_str());
+
   try {
-    client c(AF_INET, SOCK_STREAM, 0, "127.0.0.1", 8888);
+    client c(AF_INET, SOCK_STREAM, 0, server_ip, 8888);
     c.main();
   } catch (std::exception &e) {
     std::cerr << "ERROR: " << e.what() << std::endl;
   }
 
+  delete[] server_ip;
   return 0;
 }
